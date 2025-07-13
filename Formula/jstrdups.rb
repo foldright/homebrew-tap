dcd43f0ad306c487eb9e663afbe5c9f5e5ad4f7c
Jerry Lee <oldratlee@gmail.com>
2025-07-13 14:58:38 +0800

    update README

--- Formula/jstrdups.rb
+++ Formula/jstrdups.rb
@@ -1,5 +1,5 @@
 class Jstrdups < Formula
-  desc "Find out duplicate string literals in java files under current directory"
+  desc "Find out duplicate string literals in java source code files under current directory"
   homepage "https://github.com/foldright/jstrdups"
   url "https://github.com/foldright/jstrdups/releases/download/v0.2.0/jstrdups-0.2.0.zip"
   sha256 "7305e00d8255d482049294bc9a5634ac8f88aee8cbcc16ae9478f54d84305e4d"
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # homebrew-foldright
 
-[Homebrew](https://brew.sh/) tap for FoldRight.
+[Homebrew](https://brew.sh/) tap for [FoldRight](https://github.com/foldright).
+
+## Formulas
+
+- [`jstrdups`](https://github.com/foldright/jstrdups): 🔍 Find out duplicate string literals in java source code files under current directory.
+  - ```shell
+    brew install foldright/tap/jstrdups
+    ```
