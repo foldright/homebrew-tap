c28adaf7befd1ece785a681287d349a1ceddea94
Jerry Lee <oldratlee@gmail.com>
2025-07-15 14:48:08 +0800

    upgrade jstrdups to 0.2.1

--- Formula/jstrdups.rb
+++ Formula/jstrdups.rb
@@ -1,8 +1,8 @@
 class Jstrdups < Formula
   desc "Find out duplicate string literals in java source code files under current directory"
   homepage "https://github.com/foldright/jstrdups"
-  url "https://github.com/foldright/jstrdups/releases/download/v0.2.0/jstrdups-0.2.0.zip"
-  sha256 "7305e00d8255d482049294bc9a5634ac8f88aee8cbcc16ae9478f54d84305e4d"
+  url "https://github.com/foldright/jstrdups/releases/download/v0.2.1/jstrdups-0.2.1.zip"
+  sha256 "6e0b3e4f9cf7245d1e8dd725b8cca634615926ceaed717bddfcb8f32c516e6a5"
   license "Apache-2.0"
 
   def install
